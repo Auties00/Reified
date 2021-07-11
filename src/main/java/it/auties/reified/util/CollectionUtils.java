9b8c032f2ea7bba806e9f337449c5bfe03760147
Alessandro Autiero <alautiero@gmail.com>
2021-07-11 15:35:55 +0200

    Reified 1.2

---
+++ src/main/java/it/auties/reified/util/CollectionUtils.java
@@ -0,0 +1,2 @@
+package it.auties.reified.util;public class CollectionUtils {
+}
---
+++ src/main/java/it/auties/reified/util/StreamUtils.java
@@ -0,0 +1,13 @@
+package it.auties.reified.util;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.Optional;
+
+public class CollectionUtils {
+    public static <T> Optional<T> getSafe(List<T> list, int index){
+        return Optional.ofNullable(list)
+                .filter(safe -> safe.size() - 1 >= index)
+                .map(safe -> safe.get(index));
+    }
+}
